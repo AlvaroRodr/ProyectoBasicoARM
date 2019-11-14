5922d7d17456f3fc54d31deca165e780454b37bd
Álvaro Rodríguez <alvaro.rodriguez.martinez98@gmail.com>
2019-11-14 10:23:43 +0100

    metodo String

--- ProyectoBasico/src/basico/com/ProyectoBasico.java
+++ ProyectoBasico/src/basico/com/ProyectoBasico.java
@@ -1,5 +1,13 @@
 package basico.com;
 
 public class ProyectoBasico {
-
+	public static String cadenas() {
+		String cadena="Esta es una frase por defecto";
+		return cadena;
+	}
+	public static void main(String args[]) {
+		
+		System.out.print(cadenas());
+		
+	}
 }
