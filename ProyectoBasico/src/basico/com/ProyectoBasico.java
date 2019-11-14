b54832258800658eec8d4bec351cb2db1023ae6a
Álvaro Rodríguez <alvaro.rodriguez.martinez98@gmail.com>
2019-11-14 10:29:11 +0100

    metodo lógico

--- ProyectoBasico/src/basico/com/ProyectoBasico.java
+++ ProyectoBasico/src/basico/com/ProyectoBasico.java
@@ -2,4 +2,18 @@ package basico.com;
 
 public class ProyectoBasico {
 
+	public static String logico(int a, int b) {
+		String resultado=null;
+		if (a > b) {
+			resultado="\n"+a+" es mayor que "+b;
+		}else {
+			resultado="\n"+b+" es mayor que "+a;
+		}
+		return resultado;
+	} 
+	public static void main(String args[]) {
+		
+		System.out.print(logico(5,3));
+		System.out.print(logico(2,34));
+	}
 }
